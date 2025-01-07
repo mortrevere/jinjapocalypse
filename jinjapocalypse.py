fe1d66265d7c9eb6dda3e164bdf141b65fe90596
mortrevere <leo.vincent@sekoia.io>
2025-01-08 00:46:25 +0100

    _o_: add start/end_page and implement post-processor

--- jinjapocalypse.py
+++ jinjapocalypse.py
@@ -1,18 +1,51 @@
+import json
 import os
+import random
 import shutil
 import yaml
 from jinja2 import Environment, FileSystemLoader
 from loguru import logger
+import unicodedata
+import re
 
-class Toolbox():
+
+class Tokens:
+    def __init__(self):
+        self.run_id = random.randint(4, 42)
+        self.token_tmpl = f"[--- JNJPCLPS{self.run_id} -- ()]"
+
+    def bake(self, parameters):
+        baked = self.token_tmpl.replace("()", f"{json.dumps(parameters)}")
+        logger.info(baked)
+        return baked
+
+
+_TOKENS = Tokens()
+
+
+class Toolbox:
     @staticmethod
     def hourri():
         return "\\o/"
+
     def load_yaml(path):
         path = "src/" + path
         with open(path) as f:
             data = yaml.safe_load(f)
             return data
+
+    def slugify(text, delimiter="-"):
+        text = unicodedata.normalize("NFKC", text)
+        return re.sub(r"[-\s]+", delimiter, re.sub(r"[^\w\s-]", "", text).strip().lower())
+
+    def start_page(page_name):
+        page_name = Toolbox.slugify(page_name)
+        p = {"type": "start_page", "page_name": page_name}
+        return _TOKENS.bake(p)
+
+    def end_page():
+        p = {"type": "end_page"}
+        return _TOKENS.bake(p)
 
 
 class Jinjapocalypse:
@@ -89,18 +122,67 @@ class Jinjapocalypse:
                 logger.info(f"Rendering {src_file}")
                 rendered_content = self.render_template(src_file, lib_content)
 
+            sections, pure_html = self.parse_special_tags(rendered_content)
+
+            if len(sections):
+                logger.info(f"Processing {len(sections)} found in {build_file.name}")
+                self.process_sections(sections)
+
+            if not pure_html.strip():
+                logger.warning(f"Not rendering {src_file} as its final content is empty")
+                continue
+
             # Construct full path for build file
             build_file_path = os.path.join(self.build_folder, src_file)
             os.makedirs(os.path.dirname(build_file_path), exist_ok=True)
             with open(build_file_path, "w") as build_file:
-                build_file.write(rendered_content)
+                build_file.write(pure_html)
                 logger.info(f"Wrote {build_file.name}")
 
         logger.info("Copying media files ...")
-        self.copy_files(
-            self.media_folder, os.path.join(self.build_folder, self.media_folder)
-        )
+        self.copy_files(self.media_folder, os.path.join(self.build_folder, self.media_folder))
         logger.info("All done")
+
+    def process_sections(self, sections):
+        for section in sections:
+            section_type = section["opening_tag"]["type"]
+            if section_type == "start_page":
+                page_name = section["opening_tag"]["page_name"] + ".html"
+                build_file_path = os.path.join(self.build_folder, page_name)
+                os.makedirs(os.path.dirname(build_file_path), exist_ok=True)
+                with open(build_file_path, "w") as build_file:
+                    build_file.write(section["_content"])
+                    logger.info(f"Wrote {build_file.name} from section")
+
+    def parse_special_tags(self, html_str):
+        tag_pattern = re.compile(r"\[---\s*(\S+?)\s*--\s*(\{.*\})\s*\]")
+        sections, cleaned_html, stack, content_stack = [], [], [], []
+
+        for line in html_str.splitlines():
+            tag_match = tag_pattern.search(line)
+            if tag_match:
+                json_data = json.loads(tag_match.group(2))
+                if stack:
+                    last_section = stack.pop()
+                    section = {
+                        **last_section,
+                        "closing_tag": json_data,
+                        "_content": "\n".join(content_stack.pop()).strip(),
+                    }
+                    sections.append(section)
+                else:
+                    stack.append({"opening_tag": json_data})
+                    content_stack.append([])
+            elif stack:
+                content_stack[-1].append(line)
+            else:
+                cleaned_html.append(line)
+
+        if stack:
+            logger.critical(f"Found unclosed section: \n{content_stack[-1]}")
+            raise Exception("Found unclosed section.")
+
+        return sections, "\n".join(cleaned_html)
 
 
 if __name__ == "__main__":
